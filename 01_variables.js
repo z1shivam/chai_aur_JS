1daeef61e808b34bd1e5bcb43fc07ca482720122
z1shivam <shivamk.json@gmail.com>
2023-10-04 16:52:44 +0530

    variables chapter completed

--- 01_variables.js
+++ 01_variables.js
@@ -1,1 +1,19 @@
-shivam = 47
+const accountId = 14453;
+let accountEmail = 'shivam@gmail.com';
+var accountPassword = "238973";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 38377;
+// !const variable can't be changed
+
+accountEmail = "shivam22@gmail.com"
+accountPassword = "929273"
+accountCity = "Bangalore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/* 
+Prefer not to use var as var is not scope defined
+var is globally defined
+*/
---
+++ 02_dataTypes.js
@@ -0,0 +1,1 @@
+"use strict"; //will be treated as newer version of javascript
