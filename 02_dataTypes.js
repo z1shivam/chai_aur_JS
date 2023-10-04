fc62b0b9bc70ae5e21a6ee6785dd8871bc92adc9
z1shivam <shivamk.json@gmail.com>
2023-10-04 17:07:25 +0530

    datatypes completed

--- 02_dataTypes.js
+++ 02_dataTypes.js
@@ -1,1 +1,25 @@
 "use strict"; //will be treated as newer version of javascript
+
+// alert("Hello"); // we are using nodeJS, not browser
+
+console.log(3 + 4); 
+console.log("shivam")
+//code readibility should be high
+
+let name = "Shivam" //string
+let age = 19 //number = 2 to the power of 53, also bigint exist
+let isLoggedIn = true // boolean
+// null = standalone type: it is a representation of empty value
+// undefiend = abhi value assign nahi hui h
+// symbol = uniqueness
+
+// object
+
+console.log(typeof undefined);
+// ! undefined is a type but null is object
+
+/*
+    2 documentation: 
+    - tc39
+    - mdn
+*/
